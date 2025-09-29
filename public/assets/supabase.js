f496ff129c8b7106f9b37b586c81c2a671860870
joeatang <105069109+joeatang@users.noreply.github.com>
2025-09-29 13:11:31 -0400

    Create supabase.js

---
+++ public/assets/supabase.js
@@ -0,0 +1,6 @@
+<!-- public/assets/supabase.js -->
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+<script>
+  // global Supabase client
+  window.sb = supabase.createClient(window.SUPABASE_URL, window.SUPABASE_ANON_KEY);
+</script>
