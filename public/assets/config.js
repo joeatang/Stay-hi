a2cf79d2f782166e16de8f5714aece9c49a339fe
joeatang <105069109+joeatang@users.noreply.github.com>
2025-09-29 13:10:18 -0400

    Create config.js

---
+++ public/assets/config.js
@@ -0,0 +1,6 @@
+<!-- public/assets/config.js -->
+<script>
+  // TODO: paste your actual values
+  window.SUPABASE_URL = "https://gfcubvroxgfvjhacinic.supabase.co";
+  window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmY3VidnJveGdmdmpoYWNpbmljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5MTIyNjYsImV4cCI6MjA3NDQ4ODI2Nn0.5IlxofMPFNdKsEueM_dhgsJP9wI-GnZRUM9hfR0zE1g";
+</script>
